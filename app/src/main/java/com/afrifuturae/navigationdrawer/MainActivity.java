b5fc44ec90e4bf21fe7959445e40407103233186
masumba <masumbakayombo1@gmail.com>
2019-07-08 14:34:05 +0200

    Drawer layout made

--- app/src/main/java/com/afrifuturae/navigationdrawer/MainActivity.java
+++ app/src/main/java/com/afrifuturae/navigationdrawer/MainActivity.java
@@ -1,14 +1,21 @@
 package com.afrifuturae.navigationdrawer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.navigation_drawer);
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
