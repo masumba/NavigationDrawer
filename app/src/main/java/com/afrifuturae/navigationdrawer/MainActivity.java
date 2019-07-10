88caa15f2130a7e0b099154388e408d500dbf5cb
Masumba <masumbakayombo1@gmail.com>
2019-07-10 15:57:47 +0200

    Added Drawer

--- app/src/main/java/com/afrifuturae/navigationdrawer/MainActivity.java
+++ app/src/main/java/com/afrifuturae/navigationdrawer/MainActivity.java
@@ -1,13 +1,19 @@
 package com.afrifuturae.navigationdrawer;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +23,12 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView)findViewById(R.id.navigation_view);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open_drawer,R.string.close_drawer);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
 }
